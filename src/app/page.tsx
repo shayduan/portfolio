4918b0d32393ba86c16e25fd8b1fa0e476796df1
Xueyi Duan <tscharrlet@gmail.com>
2024-12-16 19:25:27 -0800

    update .env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 import { Container } from '@/components/layout/Container'
 import Newsletter from '@/components/home/Newsletter'
 import Feed from '@/components/home/Feed'
@@ -10,13 +9,18 @@ import { BlogCard } from '@/components/home/BlogCard'
 import { getAllBlogs, type BlogType } from '@/lib/blogs'
 import { ProjectCard } from '@/components/project/ProjectCard'
 import { GithubProjectCard } from '@/components/project/GithubProjectCard'
-import { projectHeadLine, projectIntro, projects, githubProjects, blogHeadLine, blogIntro, techIcons } from '@/config/infoConfig'
+import {
+  projectHeadLine,
+  projectIntro,
+  projects,
+  githubProjects,
+  blogHeadLine,
+  blogIntro,
+  techIcons,
+} from '@/config/infoConfig'
 import GithubContributions from '@/components/home/GithubCalendar'
 import { CustomIcon } from '@/components/shared/CustomIcon'
-import IconCloud from "@/components/ui/icon-cloud";
- 
-
-
+import IconCloud from '@/components/ui/icon-cloud'
 
 export default async function Home() {
   let blogList = (await getAllBlogs()).slice(0, 4)
@@ -27,16 +31,16 @@ export default async function Home() {
       <Container className="mt-9">
         {/* personal info */}
         <div className="mb-10 grid grid-cols-1 md:grid-cols-2">
-          <div className='md:mt-20'>
-            <h2 className="text-2xl font-semibold tracking-tight sm:text-3xl opacity-80">
+          <div className="md:mt-20">
+            <h2 className="text-2xl font-semibold tracking-tight opacity-80 sm:text-3xl">
               {headline}
             </h2>
             <p className="mt-6 text-base text-muted-foreground">
               {introduction}
             </p>
-            <SocialLinks className='md:mt-24'/>
+            <SocialLinks className="md:mt-24" />
           </div>
-          <div className="relative flex size-full items-center justify-center overflow-hidden w-full px-20 md:px-0 md:w-2/3 ml-auto md:mr-8">
+          <div className="relative ml-auto flex size-full w-full items-center justify-center overflow-hidden px-20 md:mr-8 md:w-2/3 md:px-0">
             <IconCloud iconSlugs={techIcons} />
           </div>
         </div>
@@ -44,11 +48,11 @@ export default async function Home() {
           <GithubContributions />
         </div>
         {/* projects */}
-        <div className="mx-auto flex flex-col max-w-xl gap-6 lg:max-w-none my-4 py-8 border-t border-muted">
-          <h2 className="text-3xl font-semibold tracking-tight md:text-5xl opacity-80">
+        <div className="mx-auto my-4 flex max-w-xl flex-col gap-6 border-t border-muted py-8 lg:max-w-none">
+          <h2 className="text-3xl font-semibold tracking-tight opacity-80 md:text-5xl">
             {projectHeadLine}
           </h2>
-          <p className="text-base text-muted-foreground max-w-2xl mb-8">
+          <p className="mb-8 max-w-2xl text-base text-muted-foreground">
             {projectIntro}
           </p>
           <ul
@@ -56,13 +60,13 @@ export default async function Home() {
             className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
           >
             {projects.map((project) => (
-              <ProjectCard key={project.name} project={project} titleAs='h3'/>
+              <ProjectCard key={project.name} project={project} titleAs="h3" />
             ))}
           </ul>
         </div>
-        <div className="mx-auto flex flex-col max-w-xl gap-6 lg:max-w-none my-4 py-8 border-t border-muted">
-          <h2 className="flex flex-row items-center justify-start gap-2 text-xl font-semibold tracking-tight md:text-3xl opacity-80 mb-4">
-            <CustomIcon name='github' size={28}/>
+        <div className="mx-auto my-4 flex max-w-xl flex-col gap-6 border-t border-muted py-8 lg:max-w-none">
+          <h2 className="mb-4 flex flex-row items-center justify-start gap-2 text-xl font-semibold tracking-tight opacity-80 md:text-3xl">
+            <CustomIcon name="github" size={28} />
             Open Source
           </h2>
           <ul
@@ -70,15 +74,19 @@ export default async function Home() {
             className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
           >
             {githubProjects.map((project) => (
-              <GithubProjectCard key={project.name} project={project} titleAs='h3'/>
+              <GithubProjectCard
+                key={project.name}
+                project={project}
+                titleAs="h3"
+              />
             ))}
           </ul>
         </div>
-        <div className="mx-auto flex flex-col max-w-xl gap-6 py-8 my-8 lg:max-w-none border-t border-muted">
-          <h2 className="text-3xl font-semibold tracking-tight md:text-5xl opacity-80">
+        <div className="mx-auto my-8 flex max-w-xl flex-col gap-6 border-t border-muted py-8 lg:max-w-none">
+          <h2 className="text-3xl font-semibold tracking-tight opacity-80 md:text-5xl">
             {blogHeadLine}
           </h2>
-          <p className="text-base text-muted-foreground max-w-2xl mb-8">
+          <p className="mb-8 max-w-2xl text-base text-muted-foreground">
             {blogIntro}
           </p>
         </div>
@@ -87,7 +95,7 @@ export default async function Home() {
           {/* blog */}
           <div className="flex flex-col gap-16">
             {blogList.map((blog: BlogType) => (
-              <BlogCard key={blog.slug} blog={blog} titleAs='h3'/>
+              <BlogCard key={blog.slug} blog={blog} titleAs="h3" />
             ))}
           </div>
 
